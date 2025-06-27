5d161f56d8a92ce47e9c1e6a725f2b8e8c14c43d
emily lobo lorenzo <emilylobo14@gmail.com>
2025-06-27 16:03:12 -0500

    feat: initialize NestJS application with TypeORM and MySQL configuration
    - Added package.json with dependencies and scripts for NestJS application.
    - Created app.module.ts to configure global settings and database connection.
    - Implemented main.ts to bootstrap the NestJS application.
    - Developed e2e tests for the application using Jest and Supertest.
    - Configured Jest for end-to-end testing with jest-e2e.json.
    - Set up TypeScript configuration files (tsconfig.json and tsconfig.build.json) for the project.

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'process';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true, envFilePath:'.env'}),
+            TypeOrmModule.forRootAsync({imports: [ConfigModule], inject: [ConfigService],useFactory: async(Config: ConfigService) => ({
+              type: 'mysql', 
+              host: Config.get('DB_HOST'), 
+              port: Config.get('DB_PORT'), 
+              username: Config.get('DB_USER'), 
+              password: Config.get('DB_PASSWORD'), 
+              database: Config.get('DB_NAME'), 
+              entities: [__dirname + '/**/.entitys/*.entity{.ts,.js}'],
+              synchronize: true,
+            })})],
+})
+export class AppModule {}
